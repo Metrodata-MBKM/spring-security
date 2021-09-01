d9090f514a5beeb9a0bb8d35c2af20f3beb5e8ab
A_Prabowo <agungprabowo8800@gmail.com>
2021-09-01 21:42:49 +0700

    initial commit

---
+++ src/main/java/com/mbkm/hr/controllers/DepartmentController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mbkm.hr.controllers;
+
+import com.mbkm.hr.models.Department;
+import java.util.List;
+
+/**
+ *
+ * @author hp
+ */
+public class DepartmentController implements BaseController<Department, Integer>{
+
+    @Override
+    public List<Department> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Department getById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Department save(Department object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Department update(Department object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String delete(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/main/java/com/mbkm/hr/models/Department.java
+++ src/main/java/com/mbkm/hr/models/Department.java
@@ -5,10 +5,31 @@
  */
 package com.mbkm.hr.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author hp
  */
+@Entity
+@Table(name = "regions")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Column(name = "department_id")
+    private int id;
     
+    @Column(name = "department_name", length = 30)
+    private String name;
+    
+    @Column(name = "manager_id", length = 30)
+    private int manager;
+    
+    @Column(name = "location_id", length = 30)
+    private int department;
 }

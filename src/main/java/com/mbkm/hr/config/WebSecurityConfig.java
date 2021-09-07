f71b6cc2c6104cc699ee93fc948dfbc17bab7d93
Wahyu Kuncoro <WahyuKu@metrodata.co.id>
2021-09-07 16:43:29 +0700

    add config and controller method global security

--- src/main/java/com/mbkm/hr/config/WebSecurityConfig.java
+++ src/main/java/com/mbkm/hr/config/WebSecurityConfig.java
@@ -7,14 +7,12 @@ package com.mbkm.hr.config;
 
 import com.mbkm.hr.services.AppUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -23,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     private AppUserDetailsService appUserDetailsService;
@@ -48,9 +47,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/region").hasAuthority("CREATE_REGION")
-                .antMatchers("/region/**").hasAnyRole("ADMIN","OPERATOR")
-                .antMatchers("/employee/**").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()

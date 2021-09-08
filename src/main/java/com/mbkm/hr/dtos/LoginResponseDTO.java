c6596b024319e7414240bd751177ad65994776bf
kevithamonding <kevithapraisy@gmail.com>
2021-09-08 23:22:55 +0800

    Add Login User

---
+++ src/main/java/com/mbkm/hr/dtos/LoginResponseDTO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mbkm.hr.dtos;
+
+import com.mbkm.hr.models.Role;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Kevitha
+ */
+@Data
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+public class LoginResponseDTO {
+    private String accessToken;
+    private Set<Role> authorities;
+}
--- src/main/java/com/mbkm/hr/services/AuthenticationService.java
+++ src/main/java/com/mbkm/hr/services/AuthenticationService.java
@@ -11,6 +11,8 @@ package com.mbkm.hr.services;
  */
 import com.mbkm.hr.config.PasswordEncoderConfig;
 import com.mbkm.hr.dtos.ConfirmationResponse;
+import com.mbkm.hr.dtos.LoginRequestDTO;
+import com.mbkm.hr.dtos.LoginResponseDTO;
 import com.mbkm.hr.dtos.RegisterRequest;
 import com.mbkm.hr.dtos.RegisterResponse;
 import com.mbkm.hr.models.User;
@@ -19,11 +21,14 @@ import com.mbkm.hr.models.Role;
 import com.mbkm.hr.repositories.RoleRepository;
 import com.mbkm.hr.repositories.UserRepository;
 import com.mbkm.hr.repositories.VerificationTokenRepository;
+import java.nio.charset.Charset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AuthenticationService {
@@ -55,14 +60,33 @@ public class AuthenticationService {
 
         return new RegisterResponse().generate(userRepository.save(user));
     }
+    
+    public LoginResponseDTO login(LoginRequestDTO request){
+        User user = userRepository.findByUsernameOrEmail(request.getUsername(), request.getUsername());
+        
+        if (!encoder.matches(request.getPassword(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong Password!");
+        }
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User Not Found!");
+        }
+        
+        return new LoginResponseDTO(createLoginToken(request.getUsername(), request.getPassword()), user.getRoles());
+    }
+    
+    public String createLoginToken(String identity, String password){
+        String auth = identity + ":" + password;
+        byte[] encodedAuth = Base64.getEncoder().encode(
+                auth.getBytes(Charset.forName("US-ASCII"))
+        );
+
+        String authHeader = "Basic " + new String(encodedAuth);
+        return authHeader;
+    }
 
     public void createVerificationToken(User user, String token) {
         VerificationToken myToken = new VerificationToken(token, user);
         tokenRepository.save(myToken);
-    }
-
-    public VerificationToken getVerificationToken(String VerificationToken) {
-        return tokenRepository.findByToken(VerificationToken);
     }
 
     public ConfirmationResponse confirmRegistration(String token){
